9871b8b8f4c511c8020779c82af5e8e843319667
joethyr <joethayr@gmail.com>
2022-12-15 08:53:32 -0700

    create game start module

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const gameBoard = {
-    const board = Array.apply(null, { length: 9 })
+const playButton = document.getElementById("btn-play");
+const boardDom = document.querySelector("table");
 
-};
+const game = (() => {
+    const start = () => {
+        playButton.style.display = "none";
+        boardDom.style.display = "inline-block";
+        document.getElementById("game-text-box").style.display = "block";
+    };
+    return {
+        start,
+    };
+})();
 
+playButton.addEventListener("click", game.start);
+
+
+
+
+// const player = (name, symbol) => {
+//     const create = () => {
+
+//     }
+
+// };
+
+// const gameBoard = {
+//     const board = Array.apply(null, { length: 9 })
+
+// };
