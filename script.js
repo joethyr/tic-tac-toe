a7a3897e4266488aabd5f376a1540c33ac0824f5
joethyr <joethayr@gmail.com>
2022-12-16 21:15:04 -0700

    remove function for players to choose name and symbol

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 const playButton = document.getElementById("btn-play");
 const boardDom = document.querySelector("table");
+const gameDialogue = document.querySelector(".game-dialogue");
+const player1 = {}
+const player2 = {}
 
 const game = (() => {
     const start = () => {
         playButton.style.display = "none";
         boardDom.style.display = "inline-block";
         document.getElementById("game-text-box").style.display = "block";
+        const enterNameText = "please enter your name and choose which symbol you'll play as"
+        // runs function that generates dialogue that asks PLAYER 1 for name and symbol
+        gameDialogue.innerHTML = `Player 1, ${enterNameText}`
     };
     return {
         start,
     };
 })();
 
-playButton.addEventListener("click", game.start);
-
-
-
-
-// const player = (name, symbol) => {
-//     const create = () => {
-
-//     }
-
-// };
-
 // const gameBoard = {
 //     const board = Array.apply(null, { length: 9 })
 
+
 // };
+playButton.addEventListener("click", game.start);
