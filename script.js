ed4a31f0a14a71733e768ae33cce8bbef720c08e
joethyr <joethayr@gmail.com>
2022-12-17 21:00:08 -0700

    add event listerner that targets specific zone

--- script.js
+++ script.js
@@ -1,26 +1,76 @@
 const playButton = document.getElementById("btn-play");
 const boardDom = document.querySelector("table");
-const gameDialogue = document.querySelector(".game-dialogue");
-const player1 = {}
-const player2 = {}
+const dialogueBox = document.querySelector(".game-dialogue");
+const zones = document.querySelectorAll(".zone");
 
-const game = (() => {
+const domController = (() => {
     const start = () => {
         playButton.style.display = "none";
         boardDom.style.display = "inline-block";
         document.getElementById("game-text-box").style.display = "block";
-        const enterNameText = "please enter your name and choose which symbol you'll play as"
-        // runs function that generates dialogue that asks PLAYER 1 for name and symbol
-        gameDialogue.innerHTML = `Player 1, ${enterNameText}`
-    };
+    }
+
+    zones.forEach((zone) => {
+        zone.addEventListener("click", (e) => {
+            console.log(e.target.dataset.index)
+            gameController.play(e.target.dataset.index)
+        })
+    })
+
     return {
-        start,
+        start
+    }
+})()
+
+const Player = (symbol) => {
+    const getSymbol = () => symbol
+
+    return {
+        getSymbol
+    }
+}
+
+const gameBoard = (() => {
+    const board = new Array(9)
+
+    const setZone = (index, symbol) => {
+        board[index] = symbol;
+    };
+
+    const getZone = (index) => board[index];
+    return {
+        board, setZone, getZone
+    }
+})()
+
+const gameController = (() => {
+    const playerX = Player("X")
+    const playerO = Player("O")
+    let turn = true
+
+    const getCurrentPlayerSymbol = () => {
+        if (turn) {
+            playerO.getSymbol()
+        } else {
+            playerX.getSymbol()
+        }
+        turn = !turn;
+    }
+
+    const play = (zoneIndex) => {
+        gameBoard.setZone(zoneIndex, getCurrentPlayerSymbol())
+
+        // code to check if a player won
+        // code to check if its a draw
+    }
+
+
+    playButton.addEventListener("click", () => {
+        domController.start()
+        play()
+    })
+
+    return {
+        play
     };
 })();
-
-// const gameBoard = {
-//     const board = Array.apply(null, { length: 9 })
-
-
-// };
-playButton.addEventListener("click", game.start);
