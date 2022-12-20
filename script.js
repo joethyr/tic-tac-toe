05bc2e91cc0c55c763a0ffab1943b427d3ee1118
joethyr <joethayr@gmail.com>
2022-12-19 21:19:13 -0700

    add reset button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const playButton = document.getElementById("btn-play");
-const boardDom = document.querySelector("table");
-const dialogueBox = document.querySelector(".game-dialogue");
-const zones = document.querySelectorAll(".zone");
-
 const domController = (() => {
+    const playButton = document.getElementById("btn-play");
+    const boardDom = document.querySelector("table");
+    const dialogueBox = document.querySelector(".game-dialogue");
+    const resetButton = document.getElementById("btn-reset");
+    const zones = document.querySelectorAll(".zone");
+    const icons = document.querySelectorAll("i")
+
+
     const start = () => {
         playButton.style.display = "none";
         boardDom.style.display = "inline-block";
@@ -12,15 +15,35 @@ const domController = (() => {
 
     zones.forEach((zone) => {
         zone.addEventListener("click", (e) => {
-            console.log(e.target.dataset.index)
-            gameController.play(e.target.dataset.index)
+            if (e.target.textContent !== "") return;
+            gameController.play(parseInt(e.target.dataset.index, 10))
         })
     })
 
+    const updateGameboard = () => {
+        for (let i = 0; i < zones.length; i++) {
+            zones[i].textContent = gameBoard.getZone(i);
+        }
+    };
+
+    const reset = () => {
+        zones.forEach((e) => {
+            e.textContent = ""
+        })
+    }
+
+    resetButton.addEventListener("click", () => {
+        reset()
+        gameBoard.reset()
+    })
+
+
+
     return {
-        start
+        start, playButton, dialogueBox, icons, updateGameboard
     }
 })()
+
 
 const Player = (symbol) => {
     const getSymbol = () => symbol
@@ -38,36 +61,45 @@ const gameBoard = (() => {
     };
 
     const getZone = (index) => board[index];
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    }
     return {
-        board, setZone, getZone
+        board, setZone, getZone, reset
     }
 })()
 
 const gameController = (() => {
-    const playerX = Player("X")
     const playerO = Player("O")
+    const playerX = Player("X")
     let turn = true
 
-    const getCurrentPlayerSymbol = () => {
+    const play = (zoneIndex) => {
         if (turn) {
-            playerO.getSymbol()
+            gameBoard.setZone(zoneIndex, playerO.getSymbol())
+            domController.updateGameboard()
+            domController.dialogueBox.innerHTML = `Turn: Player ${playerX.getSymbol()}`
+            console.log(gameBoard.board)
         } else {
-            playerX.getSymbol()
+            gameBoard.setZone(zoneIndex, playerX.getSymbol())
+            domController.updateGameboard()
+            domController.dialogueBox.innerHTML = `Turn: Player ${playerO.getSymbol()}`
+            console.log(gameBoard.board)
+
         }
+        // code to check if a player won
+        // code to check if its a draw
         turn = !turn;
     }
 
-    const play = (zoneIndex) => {
-        gameBoard.setZone(zoneIndex, getCurrentPlayerSymbol())
 
-        // code to check if a player won
-        // code to check if its a draw
-    }
-
-
-    playButton.addEventListener("click", () => {
+    domController.playButton.addEventListener("click", () => {
         domController.start()
-        play()
+        domController.dialogueBox.innerHTML = `Turn: Player ${playerO.getSymbol()}`
+        // play()
     })
 
     return {
